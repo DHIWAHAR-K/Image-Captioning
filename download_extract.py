588a1913ec9447c56a88151a8c2304f91819c8a4
DHIWAHAR-K <adhithyak99@gmail.com>
2024-06-07 12:57:10 +0530

    files updated

--- download_extract.py
+++ download_extract.py
@@ -4,16 +4,19 @@ import wget
 import zipfile
 
 def download_and_extract():
-    if not os.path.exists("Flicker8k_Dataset"):
-        wget.download("https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip")
-        with zipfile.ZipFile("Flickr8k_Dataset.zip", "r") as zip_ref:
-            zip_ref.extractall(".")
-        os.remove("Flickr8k_Dataset.zip")
+    if not os.path.exists("data/Flicker8k_Dataset"):
+        os.makedirs("data", exist_ok=True)
+        wget.download("https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip", out="data")
+        with zipfile.ZipFile("data/Flickr8k_Dataset.zip", "r") as zip_ref:
+            zip_ref.extractall("data")
+        os.remove("data/Flickr8k_Dataset.zip")
         
-    if not os.path.exists("Flickr8k.token.txt"):
-        wget.download("https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip")
-        with zipfile.ZipFile("Flickr8k_text.zip", "r") as zip_ref:
-            zip_ref.extractall(".")
+    if not os.path.exists("data/Flickr8k.token.txt"):
+        os.makedirs("data", exist_ok=True)
+        wget.download("https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip", out="data")
+        with zipfile.ZipFile("data/Flickr8k_text.zip", "r") as zip_ref:
+            zip_ref.extractall("data")
+        os.remove("data/Flickr8k_text.zip")
         os.remove("Flickr8k_text.zip")
 
 if __name__ == "__main__":
